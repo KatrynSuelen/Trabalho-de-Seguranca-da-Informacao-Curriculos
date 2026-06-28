67439a5846c3a1bd13d95ba892daffb1d5d3818f
matheusgriza <matheusgriza@sou.faccat.br>
2026-06-28 17:02:10 -0300

    feat: implements backend index.ts; add docker-compose to start mysql server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import curriculoRoutes from './routes/curriculo.routes.js';
 
+const app = express();
+const port = process.env['PORT'] ?? 3000;
 
-console.debug("hello world");
+app.use(express.json());
+app.use(curriculoRoutes);
+
+app.listen(port, () => {
+  console.log(`Backend running on http://localhost:${port}`);
+});
