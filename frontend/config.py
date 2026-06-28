2f529adb215fdc17bc4156f48511bb61bb44afa4
matheusgriza <matheusgriza@sou.faccat.br>
2026-06-28 17:00:56 -0300

    feat: deprecate init_db.py and cofigs in frotnentd py app

--- frontend/config.py
+++ frontend/config.py
@@ -2,8 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key-for-csrf-protection-987654321'
-    
- 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://root:katryn@localhost:3306/sistema_curriculos'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BACKEND_URL = os.environ.get('BACKEND_URL') or 'http://localhost:3000'
 
